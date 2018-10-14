d19e88a03a4ce29b5f87d1a1382409422f066b2c
luca <lucahhai@gmail.com>
2018-10-14 10:50:55 +0800

    test pdf output

--- source/conf.py
+++ source/conf.py
@@ -20,8 +20,8 @@ import sphinx_rtd_theme
 # -- Project information -----------------------------------------------------
 
 project = 'ocg-rule'
-copyright = u'2018, 碎冰'
-author = u'碎冰'
+copyright = '2018, 碎冰'
+author = '碎冰'
 
 # The short X.Y version
 version = ''
@@ -132,6 +132,24 @@ latex_documents = [
     (master_doc, 'ocg-rule.tex', 'ocg-rule Documentation',
      '碎冰', 'manual'),
 ]
+
+latex_elements={# The paper size ('letterpaper' or 'a4paper').
+'papersize':'a4paper',# The font size ('10pt', '11pt' or '12pt').
+'pointsize':'12pt','classoptions':',oneside','babel':'',#必須
+'inputenc':'',#必須
+'utf8extra':'',#必須
+# Additional stuff for the LaTeX preamble.
+'preamble': r"""
+\usepackage{xeCJK}
+\usepackage{indentfirst}
+\setlength{\parindent}{2em}
+\setCJKmainfont{WenQuanYi Micro Hei}
+\setCJKmonofont[Scale=0.9]{WenQuanYi Micro Hei Mono}
+\setCJKfamilyfont{song}{WenQuanYi Micro Hei}
+\setCJKfamilyfont{sf}{WenQuanYi Micro Hei}
+\XeTeXlinebreaklocale "zh"
+\XeTeXlinebreakskip = 0pt plus 1pt
+"""}
 
 
 # -- Options for manual page output ------------------------------------------
