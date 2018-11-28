0755477ceffc0fb85e36a45a10e52475cf53d06f
luca <lucahhai@gmail.com>
2018-11-28 14:45:21 +0800

    test theme

--- source/conf.py
+++ source/conf.py
@@ -76,7 +76,7 @@ pygments_style = None
 #
 # html_theme = "sphinx_rtd_theme"
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_theme = "sphinx_materialdesign_theme"
+html_theme = "press"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
