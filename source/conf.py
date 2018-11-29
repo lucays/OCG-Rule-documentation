1f67344bf1deaf1fa6976b736fa2ca3ba1875c22
luca <lucahhai@gmail.com>
2018-11-29 10:52:56 +0800

    .

--- source/conf.py
+++ source/conf.py
@@ -15,7 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import stanford_theme
+import sphinx_ustack_theme
 # from jupyter_sphinx_theme import *
 # init_theme()
 
@@ -76,8 +76,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "stanford_theme"
-html_theme_path = [stanford_theme.get_html_theme_path()]
+html_theme = "sphinx_ustack_theme"
+# html_theme_path = [stanford_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
