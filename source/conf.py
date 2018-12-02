7571d7476552fbffc3ee11ec6221b29986df22ad
luca <lucahhai@gmail.com>
2018-12-02 18:53:12 +0800

    .

--- source/conf.py
+++ source/conf.py
@@ -15,7 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
+# import sphinx_rtd_theme
 # from jupyter_sphinx_theme import *
 # init_theme()
 
@@ -75,8 +75,25 @@ pygments_style = None
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme = "sphinx_rtd_theme"
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+html_theme = 'sphinx_typlog_theme'
+html_theme_options = {
+    'logo': 'logo.jpg',
+    'description': 'OCG规则整理',
+    'github_user': 'lucays',
+    'github_repo': 'ocg-rule',
+}
+html_sidebars = {
+    '**': [
+        'logo.html',
+        'description.html',
+        'github.html',
+        'globaltoc.html',
+        'searchbox.html',
+    ]
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
