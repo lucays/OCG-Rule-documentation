00989b19e1c6d50fa966f0ee0d63696c7e26e2c6
luca <lucahhai@gmail.com>
2018-12-02 18:09:54 +0800

    .

--- source/conf.py
+++ source/conf.py
@@ -15,7 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_ustack_theme
+import sphinx_rtd_theme
 # from jupyter_sphinx_theme import *
 # init_theme()
 
@@ -75,11 +75,8 @@ pygments_style = None
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = "sphinx_rtd_theme"
-# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-html_theme = 'sphinx_ustack_theme'
-html_theme_path = [sphinx_ustack_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
