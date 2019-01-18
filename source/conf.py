73845421762aeaf8ca3bd95f40df8451b1c94e6e
luca <lucahhai@gmail.com>
2019-01-18 22:24:16 +0800

    copyright 2018→2019

--- source/conf.py
+++ source/conf.py
@@ -24,7 +24,7 @@ import datetime
 # -- Project information -----------------------------------------------------
 
 project = 'ocg-rule'
-copyright = '2018, 碎冰'
+copyright = f'2018-{datetime.datetime.now().strftime("%Y")}, 碎冰'
 author = '碎冰'
 
 # The short X.Y version
