cf91d7c342d4e825ec89dcddd20283f5b54c05a2
luca <lucahhai@gmail.com>
2019-08-11 23:41:56 +0800

    add pdf cjk extension

--- docs/conf.py
+++ docs/conf.py
@@ -42,8 +42,7 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinxcontrib.cjk']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['.templates']
