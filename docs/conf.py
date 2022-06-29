449e426726c6a4d9d57cbb910d1abeec8be61063
luca <lucahhai@gmail.com>
2022-06-29 23:28:01 +0800

    feat: open link in a new tab

--- docs/conf.py
+++ docs/conf.py
@@ -105,6 +105,10 @@ html_css_files = [
     'css/custom.css',
 ]
 
+html_js_files = [
+    'js/custom.js'
+]
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
