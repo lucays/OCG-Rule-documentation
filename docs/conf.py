b52079d9abf642373a9d2b38c13ade8ab329019c
luca <lucahhai@gmail.com>
2022-08-07 13:00:07 +0800

    fix: search

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,7 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_search.extension',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
