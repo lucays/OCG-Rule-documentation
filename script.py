831735749faf2d06bd16df341966102eb553995f
luca <lucahhai@gmail.com>
2023-04-24 23:55:50 +0800

    fix series

--- script.py
+++ script.py
@@ -1,6 +1,16 @@
 
 import re
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
+
+import requests
+
+current_dir = Path(__file__).parent.resolve()
+
+ALL_CARD_URLS, NEW_CARD_URLS, VALID_CARD_URLS = set(), set(), set()
+VALID_CARD_URLS_FILE = current_dir / 'valid_card_urls.txt'
+if VALID_CARD_URLS_FILE.exists():
+    VALID_CARD_URLS = set(VALID_CARD_URLS_FILE.read_text().split())
 
 NEED_REPLACED_NAMES = {
     '「E·HERO': '「元素英雄',
@@ -49,6 +59,7 @@ NEED_REPLACED_NAMES = {
 NOT_CARD_NAMES = (
     '攻击力',
     '守备力',
+    '等级',
     '原本攻击力',
     '原本守备力',
     '攻击力·守备力',
@@ -92,33 +103,46 @@ def add_cdb_url(texts: str) -> str:
             return True
         return False
 
+    def get_names_dict(names: list):
+        names_dict = {}
+        for name in names:
+            name = name[1:-1].strip('`_')
+            names_dict[f'「{name}」'] = f'「`{name}`_」'
+        return names_dict
+
     stack = []
-    card_names, card_name = [], []
+    cards_name, series_name, name_chars = [], [], []
     for line in texts.split('\n'):
         if need_skip(line):
             continue
-        for char in line:
+        for index, char in enumerate(line):
             if char == '「':
                 stack.append(char)
             if stack:
-                card_name.append(char)
+                name_chars.append(char)
             if char == '」':
                 stack.pop()
                 if not stack:
-                    name = ''.join(card_name)
+                    name = ''.join(name_chars)
                     is_card = True
                     for not_card_name in NOT_CARD_NAMES:
                         if not_card_name in name:
                             is_card = False
                             break
+                    for serie in ('怪兽', '魔法', '陷阱', '卡'):
+                        if line[index+1:].startswith(serie) and not line[index+1:].startswith('卡的发动'):
+                            series_name.append(name)
+                            is_card = False
+                            break
+                    if '衍生物' in name:
+                        series_name.append(name)
+                        is_card = False
                     if is_card:
-                        card_names.append(name)
-                    card_name = []
+                        cards_name.append(name)
+                    name_chars = []
 
-    card_names_dict = {}
-    for card_name in card_names:
-        card_name = card_name[1:-1].strip('`_')
-        card_names_dict[f'「{card_name}」'] = f'「`{card_name}`_」'
+    cards_name_dict = get_names_dict(cards_name)
+    series_name_dict = get_names_dict(series_name)
 
     have_url_card_names = set(re.findall('.. _`(.*?)`: ', texts))
     tail_texts = set()
@@ -128,20 +152,46 @@ def add_cdb_url(texts: str) -> str:
         if need_skip(line):
             new_texts.append(line)
             continue
-        for old, new in card_names_dict.items():
+        for old, new in cards_name_dict.items():
             line = line.replace(old, new)
         new_texts.append(line)
+    new_texts = '\n'.join(new_texts)
 
-    for card_name in card_names_dict:
+    for card_name in cards_name_dict:
         card_name = card_name[1:-1].strip('`_')
         if card_name not in have_url_card_names:
-            tail_texts.add(f'.. _`{card_name}`: https://ygocdb.com/card/name/{card_name.replace(" ", "%20")}')
+            new_card_url = f'https://ygocdb.com/card/name/{card_name.replace(" ", "%20")}'
+            tail_texts.add(f'.. _`{card_name}`: {new_card_url}')
+            NEW_CARD_URLS.add(new_card_url)
+    for serie_name in series_name_dict:
+        serie_name = serie_name[1:-1].strip('`_')
+        new_serie_url = f'https://ygocdb.com/?search={serie_name}'
+        if serie_name not in have_url_card_names:
+            tail_texts.add(f'.. _`{serie_name}`: {new_serie_url}')
+        else:
+            new_texts = new_texts.replace(f'.. _`{serie_name}`: https://ygocdb.com/card/name/{serie_name}', f'.. _`{serie_name}`: {new_serie_url}')
 
     if not have_url_card_names and tail_texts:
-        new_texts.append('\n')
+        new_texts += '\n'
     if tail_texts:
-        new_texts.extend(tail_texts)
-    return '\n'.join(new_texts).strip() + '\n'
+        new_texts += '\n'.join(tail_texts)
+    return new_texts + '\n'
+
+
+def exract_card_urls(texts: str):
+    card_urls = re.findall(': (https://ygocdb.com/card/name/.*)', texts)
+    for card_url in card_urls:
+        ALL_CARD_URLS.add(card_url)
+
+
+def check_card_urls(card_urls):
+    for card_url in card_urls:
+        r = requests.get(card_url)
+        if '没有找到对应卡片' in r.text:
+            print(card_url, 'ERROR: no card!')
+        else:
+            VALID_CARD_URLS.add(card_url)
+    VALID_CARD_URLS_FILE.write_text('\n'.join(VALID_CARD_URLS))
 
 
 def strike_completion(texts: str) -> str:
@@ -164,6 +214,7 @@ def do_one(file: Path) -> None:
     texts = add_cdb_url(texts)
     texts = add_jp_locale_in_db_url(texts)
     texts = strike_completion(texts)
+    exract_card_urls(texts)
     if texts != old_texts:
         file.write_text(texts, encoding='utf8', newline='\n')
 
@@ -177,7 +228,9 @@ def do_all() -> None:
             for file in sub_path.iterdir():
                 if file.name.endswith('.rst'):
                     do_one(file)
+    check_card_urls(NEW_CARD_URLS)
 
 
 if __name__ == '__main__':
     do_all()
+    check_card_urls(ALL_CARD_URLS)
