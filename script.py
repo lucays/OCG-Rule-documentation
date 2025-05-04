c7e1eabcfe77e9a0bee8342980c06990c8ff8515
luca <luca@luca-PC>
2025-05-05 01:52:29 +0800

    正文内容补充

--- script.py
+++ script.py
@@ -330,7 +330,7 @@ def do_git():
     repo.git.checkout('dev')
     if repo.is_dirty():
         repo.git.add('.')
-        repo.index.commit('add faq')
+        repo.index.commit('正文内容补充')
         repo.git.push()
 
     if 'valid' in repo.branches:
